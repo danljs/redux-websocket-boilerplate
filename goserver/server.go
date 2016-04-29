0a65ceec2df9d1af06f67f338b7fa1d8d9331029
danielliu1973 <daniel.liu1973@gmail.com>
2016-04-29 17:58:26 -0400

    go

---
+++ goserver/server.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	// "io"
+	"log"
+	"net/http"
+	"golang.org/x/net/websocket"
+)
+
+func echoHandler(ws *websocket.Conn) {
+	// io.Copy(ws, ws)
+	msg := make([]byte, 512)
+	for {
+		n, err := ws.Read(msg)
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Printf("Receive: %s\n", msg[:n])
+
+		m, err := ws.Write(msg[:n])
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("Send: %s\n", msg[:m])
+	}
+}
+
+func main() {
+	http.Handle("/server", websocket.Handler(echoHandler))
+	http.Handle("/", http.FileServer(http.Dir(".")))
+	err := http.ListenAndServe(":8585", nil)
+	if err != nil {
+		panic("ListenAndServe: " + err.Error())
+	}
+}
