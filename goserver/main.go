188523a0e5418965b1b62f05bc37216efbd55619
danielliu1973 <daniel.liu1973@gmail.com>
2016-05-12 18:04:51 -0400

    prepare to build a new app

--- goserver/main.go
+++ goserver/main.go
@@ -1,20 +1,24 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"./controller"
 )
 
 func main() {
+	fmt.Printf("1\n")
 	log.SetFlags(log.Lshortfile)
-
+	fmt.Printf("2\n")
 	// websocket server
 	server := controller.NewServer("/server")
+	fmt.Printf("3\n")
 	go server.Listen()
-
+	fmt.Printf("4\n")
 	// static files
 	// http.Handle("/", http.FileServer(http.Dir("webroot")))
 
 	log.Fatal(http.ListenAndServe(":8585", nil))
+	fmt.Printf("5\n")
 }
